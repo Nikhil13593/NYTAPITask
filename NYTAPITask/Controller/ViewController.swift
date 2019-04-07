ba8f0045d49294e779040e2ccc5ef07d60568131
Nikhil Patil <npatil972@gmail.com>
2019-04-08 00:05:57 +0530

    Small adjustment in constraints.Remove unnecessary code line ,extra descriptive comments replace with Prior Comments.[Final Commit.]

--- NYTAPITask/Controller/ViewController.swift
+++ NYTAPITask/Controller/ViewController.swift
@@ -28,48 +28,41 @@ class ViewController: UIViewController{
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        tableView.estimatedRowHeight = 450.0  //Give an approximation here
-        tableView.rowHeight = UITableView.automaticDimension
         self.serverCommunication()
     }
 }
+
 // MARK:- Function for ServerCommunication
 extension ViewController{
     
     func serverCommunication(){
+        
         urlRequest = URLRequest(url: URL(string: "https://api.nytimes.com/svc/topstories/v2/science.json?api-key=zuqMni6rqwy7i97bAhoAdJZXSEtCzZGg")!)
         
         urlRequest.httpMethod = "GET"
         
         URLSession.shared.dataTask(with: urlRequest) { (alldata, resp, err) in
-            
-            print("\(alldata)")
-            
             do{
-                
                 self.resultData = try JSONDecoder().decode(Model.self, from: alldata!)
                 
-                var serverData = self.resultData.results
-                
-                for i in 0..<self.resultData.results.count{
+                for i in 0..<self.resultData.results.count
+                {
                     self.titleArray.append(self.resultData.results[i].title!)
                     self.abstractArray.append(self.resultData.results[i].abstract!)
                 self.publishedDateArray.append(self.resultData.results[i].published_date!)
                     
                     let multiMedia = self.resultData.results[i].multimedia[3].url
-                    let compright = self.resultData.results[i].multimedia[3].copyright
+                    self.urlImageArray.append(multiMedia!)
                     
-                    self.urlImageArray.append(multiMedia as! String)
-                    self.copyright.append(compright as! String)
+                    let copyright = self.resultData.results[i].multimedia[3].copyright
+                    self.copyright.append(copyright!)
                 }
                 
                 DispatchQueue.main.async {
                     self.tableView.reloadData()
                 }
-                
             }
-            catch
-            {
+            catch{
                 print("Unable to Get Respond from Server")
             }
         }.resume()
@@ -79,6 +72,7 @@ extension ViewController{
 // MARK:- UITableView Delegate & Data Source
 extension ViewController: UITableViewDelegate, UITableViewDataSource{
    
+    //Automatically Adjusting row height by internal Data.
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return UITableView.automaticDimension
     }
@@ -86,36 +80,34 @@ extension ViewController: UITableViewDelegate, UITableViewDataSource{
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return titleArray.count
     }
+    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
 
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell") as! TableViewCell
+        
         cell.titleLbl.text = titleArray[indexPath.row]
+        
         cell.abstractLbl.text = abstractArray[indexPath.row]
         
+        //MARK: In the place of .image used CocoaPod .sd_setImage code for faster Image Load
         let URLString =  urlImageArray[indexPath.row]
+        cell.imageURLOfImageView.sd_setImage(with: URL(string: "\(URLString)"), placeholderImage: UIImage(named: "placeholder.png"))
         
         cell.copyrightsLbl.text = copyright[indexPath.row]
         
-        cell.imageURLOfImageView.sd_setImage(with: URL(string: "\(URLString)"), placeholderImage: UIImage(named: "placeholder.png"))
-        
-        //Here We Created a Date Formater
+        // Date Formater for change json Date in string
         let dateFormatter = DateFormatter()
-        // Giving the format for which format you want the date
         dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZ"
-        // Here we are converting String Date to Date Formate which we already mention formate and Storing in variable acording to index path
         let dateManual = dateFormatter.date(from:publishedDateArray[indexPath.row])
-        //Again Created a Date Formater Variable
+        
+        //MARK:(Extra/New) Date Formatter to convert Date data(String) in own Date Format
         let NewdateFormatter = DateFormatter()
-        // Given the New formate for Recieved date
         NewdateFormatter.dateFormat = "MMMM dd"
-        // Converting Date to String Format which we taken Update & Storing in Variable
         let newDate = NewdateFormatter.string(from: dateManual!)
+        
         cell.publishedDateLbl.text = newDate
 
         return cell
     }
     
-    
-    
 }
-
